1ac0738a33855a8b099a3d735940f2e51075c179
Ommagatto <266509656+ommagatto@users.noreply.github.com>
2026-08-25 20:16:46 +0300

    Add header and start free trial assertions

--- test/pageobjects/github/pricing.page.js
+++ test/pageobjects/github/pricing.page.js
@@ -1,7 +1,36 @@
 class PricingPage {
 
+    pageURL = "https://github.com/pricing"
+    
+    get header() { return $('h1[class="h2-mktg"]') }
+    get startFreeTrialButton() { return $('a[class="d-block btn-mktg btn-free-trial px-2"]') }
+
+
+    async checkPageURL() {
+        await expect(browser).toHaveUrl(this.pageURL)
+    }
+
+    async assertHeaderText() {
+        await expect(this.header).toHaveText("Try the Copilot-powered platform")
+    }
+
+
+    async scrollTostartFreeTrialButton() {
+        await this.startFreeTrialButton.scrollIntoView({
+            block: "center",
+        });
+    };
+
+
+    async isStartFreeTrialButtonDisplayed() {
+        await this.startFreeTrialButton.isDisplayed({ withinViewport: true })
+    }
+
+
+    async clickStartFreeTrialButton() {
+        await this.startFreeTrialButton.click()
+    }
+
 };
 
-
-    
 export default new PricingPage()
