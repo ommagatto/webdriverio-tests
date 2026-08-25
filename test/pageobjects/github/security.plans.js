75a9b330f83d62682e3033d4c32b350c7691a2ca
Ommagatto <266509656+ommagatto@users.noreply.github.com>
2026-08-25 16:41:47 +0300

    Fix: Replace brittle selectors with IDs

--- test/pageobjects/github/security.plans.js
+++ test/pageobjects/github/security.plans.js
@@ -1,6 +1,6 @@
 class SecurityPlansPage {
 
-    get plansHeader()   { return $('h1[class="Primer_Brand__Heading-module__Heading___fudyh Primer_Brand__Heading-module__Heading-font--mona-sans___0OM_a Primer_Brand__Heading-module__Heading--1___O9POI Primer_Brand__Heading-module__Heading--textWrap-balance___XJ9OD Primer_Brand__Hero-module__Hero-heading___HMve3"]') }
+    get plansHeader() { return $('h1[id="hero-section-brand-heading"]') }
     
     
     async assertHeaderText() {
