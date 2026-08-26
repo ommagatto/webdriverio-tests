9d9513c431b300f5b85b2cf0d7cddb7e31c2f634
Ommagatto <266509656+ommagatto@users.noreply.github.com>
2026-08-26 18:07:25 +0300

    Add pricing button locator and click action to header PageObject

--- test/pageobjects/github/header.js
+++ test/pageobjects/github/header.js
@@ -1,8 +1,13 @@
 class Header {
 
+    get pricingButton() { return $('//header/div/div[2]/div/nav/ul/li[6]/a') }
     get searchButton() { return $('button[aria-label="Search or jump to, type / to search"]') }
     get searchInput() { return $('input[aria-label="Search or jump to"]') }
 
+
+    async clickPricingButton() {
+        await this.pricingButton.click()
+    }
 
     async clickSearchButton() {
         await this.searchButton.click()
